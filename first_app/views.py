8844d1ee136ed00baac9083dc272cdffba87a7d1
pro <pro@prodeMacBook-Pro-2.local>
2023-07-30 00:05:36 +0600

    about and contact page show

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def contact(request):
+    return HttpResponse("This is contact page that is created in first app.")
+
+
+def about(request):
+    return HttpResponse("This is about page that is created in first app.")
--- second_project/urls.py
+++ second_project/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    # path('first_app/', include('first_app.urls')),
+    path('first_app/', include('first_app.urls')),
     # path('second_app/', include('second_app.urls')),
 ]
